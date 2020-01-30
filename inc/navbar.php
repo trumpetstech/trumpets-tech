c228e1616cbc7bd77809753b1532a20ca58aaaf4
Trumpets <operations@trumpetstechnologies.com>
2020-01-30 12:29:51 +0530

    wip

--- inc/navbar.php
+++ inc/navbar.php
@@ -24,7 +24,7 @@
                                         </li>
                                         <li><a href="about.php">Company</a></li>
                                             <!-- submenu  -->
-                                                 <li><a href="service.php">Services</a></li>
+                                              <!--    <li><a href="service.php">Services</a></li> -->
                                         <li><a href="products.php">Products</a></li>
                                        <!--  <li class="submenu dropdown mega-dropdown" style="">
                                              <a href="products.php" class="dropdown-toggle" data-toggle="dropdown">Products <span class="caret"></span></a>
