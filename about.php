6749960f6a840610e1123425f2b23d3dbff2689e
Trumpets <operations@trumpetstechnologies.com>
2020-01-31 11:45:08 +0530

    wip

--- about.php
+++ about.php
@@ -283,7 +283,7 @@ include"inc/navbar.php"
                     <div class="col-lg-12">
                         <div class="section-title-wrap text-center section-space--mb_40">
                             <h6 class="section-sub-title mb-20">Testimonials</h6>
-                            <h3 class="heading">What do people praise about <span class="text-color-primary"> Mitech?</span></h3>
+                            <h3 class="heading">What do people praise about <span class="text-color-primary"> Trumpets?</span></h3>
                         </div>
                         <div class="testimonial-slider">
                             <div class="swiper-container testimonial-slider__container">
@@ -305,13 +305,13 @@ include"inc/navbar.php"
                                                         <span class="fa fa-star"></span>
                                                     </div>
                                                     <div class="author-info">
-                                                        <h6 class="name">Abbie Ferguson</h6>
-                                                        <span class="designation">Marketing</span>
+                                                        <h6 class="name">Karan Patil</h6>
+                                                        <span class="designation">Cheminova Agro Pvt. Ltd.</span>
                                                     </div>
                                                 </div>
                                             </div>
 
-                                            <div class="testimonial-slider__text">I’ve been working with over 35 IT companies on more than 200 projects of our company, but @Mitech is one of the most impressive to me.</div>
+                                            <div class="testimonial-slider__text">Platforms provided by Trumpets have made our industry's output to a more better entext and have simplied a lot of our process.</div>
 
                                         </div>
                                     </div>
@@ -332,13 +332,13 @@ include"inc/navbar.php"
                                                         <span class="fa fa-star"></span>
                                                     </div>
                                                     <div class="author-info">
-                                                        <h6 class="name">Monica Blews</h6>
-                                                        <span class="designation">Web designer</span>
+                                                        <h6 class="name"> Gaurav Pangahvane</h6>
+                                                        <span class="designation">Bhrama Valley</span>
                                                     </div>
                                                 </div>
                                             </div>
 
-                                            <div class="testimonial-slider__text">I’ve been working with over 35 IT companies on more than 200 projects of our company, but @Mitech is one of the most impressive to me.</div>
+                                            <div class="testimonial-slider__text">We needed a minimal solution to manage our grocery inventory and Invento from Trumpets got to be the perfect choice.</div>
 
                                         </div>
                                     </div>
@@ -359,13 +359,13 @@ include"inc/navbar.php"
                                                         <span class="fa fa-star"></span>
                                                     </div>
                                                     <div class="author-info">
-                                                        <h6 class="name">Abbie Ferguson</h6>
-                                                        <span class="designation">WEB DESIGNER</span>
+                                                        <h6 class="name">Shekhar Bam</h6>
+                                                        <span class="designation"> Surya Group</span>
                                                     </div>
                                                 </div>
                                             </div>
 
-                                            <div class="testimonial-slider__text">I’ve been working with over 35 IT companies on more than 200 projects of our company, but @Mitech is one of the most impressive to me.</div>
+                                            <div class="testimonial-slider__text">Enqubyte has eleminated a lot of our manual and paperwork and has brought up a huge impact on our company workflow.</div>
 
                                         </div>
                                     </div>
@@ -386,8 +386,8 @@ include"inc/navbar.php"
                                                         <span class="fa fa-star"></span>
                                                     </div>
                                                     <div class="author-info">
-                                                        <h6 class="name">Abbie Ferguson</h6>
-                                                        <span class="designation">WEB DESIGNER</span>
+                                                        <h6 class="name">Himanshu Ratnaparkhi</h6>
+                                                        <span class="designation">CEO, Solicitious</span>
                                                     </div>
                                                 </div>
                                             </div>
@@ -547,7 +547,7 @@ include"inc/navbar.php"
                                             </div>
                                             <div class="main-content">
                                                 <h6 class="heading">Call for advice now!</h6>
-                                                <div class="text">1900 68668</div>
+                                                <div class="text">+91 9922367414</div>
                                             </div>
                                         </div>
                                     </div>
@@ -560,7 +560,7 @@ include"inc/navbar.php"
                                             </div>
                                             <div class="main-content">
                                                 <h6 class="heading">Say hello</h6>
-                                                <div class="text">hello@mitech.com</div>
+                                                <div class="text">hello@Trumpets.co.in</div>
                                             </div>
                                         </div>
                                     </div>
