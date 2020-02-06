45932f3342acdcec7c36a0b59cf1a1ad1d152b2b
Trumpets <operations@trumpetstechnologies.com>
2020-02-06 17:26:05 +0530

    wip

--- index.php
+++ index.php
@@ -535,7 +535,7 @@ include"inc/navbar.php";
                                             </div>
                                             <div class="content">
                                                 <h6 class="heading">Human Resource Tools</h6>
-                                                <div class="post-categories">Cyber Security</div>
+                                                <div class="post-categories">Enterprise Application</div>
                                                 <div class="text">Our human resource tools improvises and automates the employee management process in an organization.
                                                 </div>
                                                 <div class="box-projects-arrow">
@@ -557,7 +557,7 @@ include"inc/navbar.php";
                                             </div>
                                             <div class="content">
                                                 <h6 class="heading">Customer Relationship Tools</h6>
-                                                <div class="post-categories">Cyber Security</div>
+                                                <div class="post-categories">Enterprise Application</div>
                                                 <div class="text">Managing leads & customer data is a crucial process for oragnization. We provide tools to make this process efficient and hassle-free.
                                                 </div>
                                                 <div class="box-projects-arrow">
@@ -601,7 +601,7 @@ include"inc/navbar.php";
                                             </div>
                                             <div class="content">
                                                 <h6 class="heading">Video Conferencing Tools</h6>
-                                                <div class="post-categories">Cyber Security</div>
+                                                <div class="post-categories">Enterprise Application</div>
                                                 <div class="text">We provide tools with a meeting space where teams can  video call, chat, meet, share files and collaborate simultaneously.
                                                 </div>
                                                 <div class="box-projects-arrow">
@@ -623,7 +623,7 @@ include"inc/navbar.php";
                                             </div>
                                             <div class="content">
                                                 <h6 class="heading">Retail POS Software</h6>
-                                                <div class="post-categories">Cyber Security</div>
+                                                <div class="post-categories">Enterprise Application</div>
                                                 <div class="text">We tune in modern cloud based POS platforms that enable retails store to manage their business from anywhere with fesibility.
                                                 </div>
                                                 <div class="box-projects-arrow">
@@ -645,7 +645,7 @@ include"inc/navbar.php";
                                             </div>
                                             <div class="content">
                                                 <h6 class="heading">Help Desk Software</h6>
-                                                <div class="post-categories">Cyber Security</div>
+                                                <div class="post-categories">Enterprise Application</div>
                                                 <div class="text">Providing online support to customers involves numerous steps if done manually. We provide efficient tools to automate...
                                                 </div>
                                                 <div class="box-projects-arrow">
@@ -667,7 +667,7 @@ include"inc/navbar.php";
                                             </div>
                                             <div class="content">
                                                 <h6 class="heading">Self-Hosted Files Management</h6>
-                                                <div class="post-categories">Cyber Security</div>
+                                                <div class="post-categories">Enterprise Application</div>
                                                 <div class="text">We provide tools for securely storing, managing and sharing files within organizations seamlessly and efficiently.
                                                 </div>
                                                 <div class="box-projects-arrow">
